e8e40d53213bd17d7d13742d7b069abec0162813
Maykon Eduardo Pereira de Sousa <maeduardo@uolinc.com>
2023-01-13 16:09:50 -0300

    remove fundo rocket

--- src/styles/pages/app.styles.ts
+++ src/styles/pages/app.styles.ts
@@ -1,11 +1,6 @@
 import styled from "styled-components";
-import bGimage from "../../assets/bg.png";
 
 export const AppContainer = styled.div`
-  background-image: url(${bGimage.src});
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
   border: solid transparent 1px;
   padding: 2rem;
 `;
